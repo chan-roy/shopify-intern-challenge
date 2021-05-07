62b29bc9232063714733093a0067ef64d029aaa7
Roy Chan <rk.chan@mail.utoronto.ca>
2021-05-07 12:22:26 -0400

    add main component & styling, remove template files

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import './Main.css'
+
+
+function Main() {
+    return (
+        <main>
+            <div>
+                <h1>The Shoppies</h1>
+            </div>
+            <div className='container'>
+            </div>
+        </main>
+    )
+}
+
+export default Main
